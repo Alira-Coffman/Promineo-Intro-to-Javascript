48c2f1e865a624ccdf70f30183f3ae87979e4ebc
Alira-Coffman <aliradawn@gmail.com>
2022-12-06 22:07:47 -0600

    week 1 examples 11-29

---
+++ week1-2/script.js
@@ -0,0 +1,43 @@
+//print hello
+console.log("hello");
+var myStr = "hello";
+//variable named x equal to 5
+var x = 5;
+
+//print variable x
+console.log("x: ", x);
+
+//reassign x to 7
+x = 7; //data type is a number
+//output x
+console.log("x: ", x);
+
+//create a var named y equal to 3
+var y = 3;
+//create a var named z = x + y
+var z = x + y; //value is 10
+
+console.log(z);
+
+var a = myStr + x;
+console.log(a);
+
+var b = x - y; //4 
+console.log(b)
+
+b = x/y;
+console.log(b)
+
+b = x * y;
+console.log(b);
+
+b++; //b = 1 + b
+console.log(b);
+
+--b; //b = b - 1
+console.log(b);
+
+
+console.log(b % 3);
+
+console.log(b % 2);
---
+++ week1-intro/script.js
@@ -0,0 +1,6 @@
+var x = 7;
+
+//variable called test with the value of "hello"
+var test = "hello";
+
+console.log(x, test);
